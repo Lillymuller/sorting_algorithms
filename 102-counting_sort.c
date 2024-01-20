fb87995b20f2da0cf13fd0413ec358a508ba2e81
Yared91 <ykebede010@gmail.com>
2024-01-20 10:36:25 -0800

    102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,37 +10,42 @@
 void counting_sort(int *array, size_t size)
 {
 	int i;
-	int *count = 0;
+	int *count;
 	int k;
-	int *a = 0;
+	int *a;
 	int num_elem = size;
 
 	if (array == NULL || size < 2)
 		return;
-
 	for (k = array[0]; i < num_elem; i++)
 	{
 		if (array[i] > k)
 			k = array[i];
 	}
-
+	a = malloc(sizeof(int) * num_elem);
+	if (a == NULL)
+		return;
+	count = malloc(sizeof(int) * (k + 1));
+	if (count == NULL)
+	{
+		free(a);
+		return;
+	}
 	for (i = 0; i < num_elem; i++)
 	{
 		count[array[i]]++;
 	}
-
-	for (i = 1; i <= k; i++)
-	{
+	for (i = 1; i < (k + 1); i++)
 		count[i] += count[i - 1];
-	}
-
+	print_array(count, k + 1);
 	for (i = num_elem - 1; i >= 0; i--)
 	{
-		a[count[array[i]]--] = array[i];
+		a[--count[array[i]]] = array[i];
 	}
-
 	for (i = 0; i < num_elem; i++)
 	{
 		array[i] = a[i];
 	}
+	free(a);
+	free(count);
 }
