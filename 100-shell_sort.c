13945914bbda3572a7d5b34aec950f96b744f1e3
Lillymuller <eleltaalemu@gmail.com>
2024-01-20 03:52:38 -0800

    shell sorting

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,7 +9,7 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, low, up;
+	size_t gap, low, up;
 	size_t num_elem = size;
 	int tmp;
 
@@ -17,10 +17,11 @@ void shell_sort(int *array, size_t size)
 		return;
 
 	/* loop for gap */
-	for (; gap < num_elem / 3; gap = (gap * 3) + 1)
+	for (gap = 1; gap < num_elem / 3;)
+		gap = (gap * 3) + 1;
 	{
 		/* loop move from up to down wards */
-		for (; gap > 0; gap /= 3)
+		for (; gap >= 1; gap /= 3)
 		{
 			/* loop that move from lower to upper */
 			for (low = gap; low < num_elem; low++)
@@ -36,5 +37,3 @@ void shell_sort(int *array, size_t size)
 		}
 	}
 }
-
-
