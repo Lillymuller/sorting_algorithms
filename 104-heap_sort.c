5fa233e5bb6cea4977267ba101ea4dcee85a3897
Yared91 <ykebede010@gmail.com>
2024-01-21 11:01:48 -0800

    104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -5,21 +5,22 @@
  * @array: array to be sorted
  * @size: size of the array
  * @i: the max of the heap
+ * @low: the bottom of the heap
  * Return: void
  */
 
-void max_heapify(int *array, size_t size, int i)
+void max_heapify(int *array, size_t size, int i, int low)
 {
-	int num_elem = size;
+	int bottom = low;
 	int tmp;
 	int highest = i;
 	int lt = (2 * i) + 1;
 	int rt = (2 * i) + 2;
 
-	while (lt < num_elem && array[lt] > array[highest])
+	while (lt < bottom && array[lt] > array[highest])
 		highest = lt;
 
-	while (rt < num_elem && array[rt] > array[highest])
+	while (rt < bottom && array[rt] > array[highest])
 		highest = rt;
 
 	if (highest != i)
@@ -28,7 +29,7 @@ void max_heapify(int *array, size_t size, int i)
 		array[i] = array[highest];
 		array[highest] = tmp;
 		print_array(array, size);
-		max_heapify(array, size, highest);
+		max_heapify(array, size, highest, bottom);
 	}
 }
 
@@ -47,15 +48,15 @@ void heap_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = (size / 2) - 1; i >= 0; i--)
-		max_heapify(array, size, i);
+	for (i = (size / 2) - 1; i >= 1; i--)
+		max_heapify(array, size, size, i);
 
 	for (i = size - 1; i > 0; i--)
 	{
-		tmp = array[1];
-		array[1] = array[i];
-		array[i] = tmp;
+		tmp = *array;
+		*array = *(array + i);
+		*(array + i) = tmp;
 		print_array(array, size);
-		max_heapify(array, size, i);
+		max_heapify(array, size, i, 0);
 	}
 }
