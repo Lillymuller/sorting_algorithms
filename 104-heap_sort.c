d194d6ac7941e89b1000e8bf22e8523b2e5c85bc
Yared91 <ykebede010@gmail.com>
2024-01-21 04:29:08 -0800

    104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -13,13 +13,13 @@ void max_heapify(int *array, size_t size, int i)
 	int num_elem = size;
 	int tmp;
 	int highest = i;
-	int lt = 2 * i;
-	int rt = (2 * i) + 1;
+	int lt = (2 * i) + 1;
+	int rt = (2 * i) + 2;
 
-	while (lt <= num_elem && array[lt] > array[highest])
+	while (lt < num_elem && array[lt] > array[highest])
 		highest = lt;
 
-	while (rt <= num_elem && array[rt] > array[highest])
+	while (rt < num_elem && array[rt] > array[highest])
 		highest = rt;
 
 	if (highest != i)
@@ -48,13 +48,13 @@ void heap_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = num_elem / 2; i >= 1; i--)
+	for (i = (num_elem / 2) - 1; i >= 0; i--)
 		max_heapify(array, size, i);
 
-	for (i = num_elem; i > 1; i--)
+	for (i = num_elem - 1; i > 0; i--)
 	{
-		tmp = array[i];
-		array[i] = array[i];
+		tmp = *array;
+		*array = array[i];
 		array[i] = tmp;
 		print_array(array, size);
 		max_heapify(array, size, i);
