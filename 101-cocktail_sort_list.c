bc17d366e3445989a5512f4c431392b34cfb5313
Yared91 <ykebede010@gmail.com>
2024-01-22 01:05:49 -0800

    101-cocktail_sort_list.c

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ */
+
+void swap-cocktail(listint_t **list, listint_t *tmp, listint_t *top)
+/**
+ * cocktail_sort_list - sorts a doubly linked list of integers using cocktail
+ * @list: list of the sorted algorithm
+ * Return: void
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
