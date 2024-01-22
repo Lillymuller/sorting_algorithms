26444b8fe97867c35481ecd7761a76461f9fb58b
Yared91 <ykebede010@gmail.com>
2024-01-21 23:07:54 -0800

    106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,6 +1,13 @@
 #include "sort.h"
 
 /**
+ * bitonic_merge - merges the bitonic list
+ * @array: array to be sorted
+ * @size: size of the array
+ * @sequence: sequence of the bitonic list
+ * @fl: flow of the bitonic list
+ * @begin: start of the bitonic list
+ * Return: void
  */
 
 void bitonic_merge(int *array, size_t size, int sequence, char fl, int begin)
@@ -31,22 +38,30 @@ void bitonic_merge(int *array, size_t size, int sequence, char fl, int begin)
 }
 
 /**
+ * bitonic_sequence - sequence of the bitonic list
+ * @array: array to be sorted
+ * @size: size of the array
+ * @sequence: sequence list
+ * @fl: flow of the bitonic list
+ * @begin: start of the bitonic list
+ * Return: void
  */
 
-void bitonic_seq(int *array, size_t size, size_t sequence, char fl, int begin)
+void bitonic_sequence(int *array, size_t size, size_t sequence, char fl,
+	int begin)
 {
 	int div = sequence / 2;
 	int UP = 1;
 	int DOWN = 0;
 	char *string = (fl == 1) ? "UP" : "DOWN";
 
-	if (sequence > 1) 
+	if (sequence > 1)
 	{
 		printf("Merging [%lu/%lu] (%s:\n)", sequence, size, string);
 		print_array(array + begin, sequence);
 
-		bitonic_seq(array, size, div, UP, begin);
-		bitonic_seq(array, size, div, DOWN, begin + div);
+		bitonic_sequence(array, size, div, UP, begin);
+		bitonic_sequence(array, size, div, DOWN, begin + div);
 		bitonic_merge(array, size, sequence, fl, begin);
 
 		printf("Merging [%lu/%lu] (%s:\n)", sequence, size, string);
@@ -55,6 +70,10 @@ void bitonic_seq(int *array, size_t size, size_t sequence, char fl, int begin)
 }
 
 /**
+ * bitonic_sort - sorts an array of integers of bitonic sort
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: void
  */
 
 void bitonic_sort(int *array, size_t size)
@@ -64,5 +83,5 @@ void bitonic_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	bitonic_seq(array, size, size, UP, 0);
+	bitonic_sequence(array, size, size, UP, 0);
 }
